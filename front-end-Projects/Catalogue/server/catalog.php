90db3d729f28c3960b899fa0f72b5dcaddf9fb87
Gabriel <gabrielcamilo89@gmail.com>
2016-06-16 12:49:03 -0400

    Create catalog.php
    Server side

---
+++ front-end-Projects/Catalogue/server/catalog.php
@@ -0,0 +1,14 @@
+<?php
+	header("Content-Type: application/json; charset=UTF-8");
+
+	$db = new mysqli('localhost', 'Gabriel', '3peo23', 'Catalog'); // set connection
+	$result = $db->query("SELECT * FROM catalog_list"); // set queries selecting table
+	$content = array(); 
+
+	while($row = $result->fetch_array()) { // fetch content from table
+		$content[] = $row; // and equate to array	
+	}
+
+	echo json_encode($content); // convert data to JSON encoded
+	$db->close();
+	
